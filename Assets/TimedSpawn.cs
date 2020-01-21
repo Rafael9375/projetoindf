703c147c2dde30e89f9f2e6c9511fb4584798792
Rafael9375 <rafael.anexo@gmail.com>
2020-01-21 00:17:16 -0300

    seq com nome aleatório

--- Assets/TimedSpawn.cs
+++ Assets/TimedSpawn.cs
@@ -9,6 +9,7 @@ public class TimedSpawn : MonoBehaviour
     public bool selected = false;
     public float spawnTime;
     public float spawnDelay;
+    internal int nomeInt = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +17,26 @@ public class TimedSpawn : MonoBehaviour
     }
 
     public void SpawnObject(){
-        var v2 = new Vector2(4,3.4f);
-        Instantiate(spawnee[Random.Range(0, 4)], v2, transform.rotation);
+        var v2 = new Vector2(4,3.4f);        
+        var gerado = (GameObject)Instantiate(spawnee[Random.Range(0, 4)], v2, transform.rotation);
+        nomeInt++;
+        gerado.name += nomeInt;
         if(stopSpawning){
             CancelInvoke("SpawnObject");
         }
     }
 
     void Update() {
+
         if(selected){
             InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
             stopSpawning = false;
             selected = false;
         }
     }
-    
+    public void Button_Click()
+    {
+        Debug.Log("asd");
+    }
+
 }
--- Assets/mover.cs
+++ Assets/mover.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class mover : MonoBehaviour
 {
     public GameObject obj;
+    public GameObject btn;
+    float objX;
+    internal string valorFoco;
+    int op = 0;
     public void Lanca(){
         Vector2 vector2 = new Vector2(4, 3.45f);
         obj.transform.position = vector2;
@@ -19,6 +23,30 @@ public class mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        obj.transform.Translate(Vector2.left *  Time.deltaTime * 3);
+        obj.transform.Translate(Vector2.left *  Time.deltaTime * 2);
+        objX = obj.transform.position.x;
+        if(objX > -0.80f && objX < 0.80f)
+        {
+            GameObject.Find("gobj").tag = obj.tag;
+            op++;
+            Debug.Log(op + obj.name);
+        }
+        else
+        {
+            GameObject.Find("gobj").tag = "vazio";
+        }   
+
+        if(obj.transform.position.x < -3.5)
+        {
+            Destroy(obj);
+        }
+    }
+
+    public void Button_Click(GameObject f)
+    {
+        
+        var t = GameObject.Find("gobj").tag;
+        Debug.Log(t);
+        
     }
 }
